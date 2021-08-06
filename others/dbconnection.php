699984261ef20175779e43faebf8401af3d6756a
souravdn <souravdebnath.code@gmail.com>
2021-08-06 13:39:19 +0530

    commit

--- others/dbconnection.php
+++ others/dbconnection.php
@@ -1,6 +1,7 @@
 <?php
 //connect to db
-$con=new mysqli("localhost","root","");
+
+$con=new mysqli("mysql-43037-0.cloudclusters.net","admin","6DCiZH15");
 if(!$con){
     // echo "db connection failed";
 }
