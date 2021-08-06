c8bd6d13f91f7892bac963a2093a66cca61a4580
souravdn <souravdebnath.code@gmail.com>
2021-08-06 13:48:01 +0530

    Update dbconnection.php

--- others/dbconnection.php
+++ others/dbconnection.php
@@ -1,7 +1,7 @@
 <?php
 //connect to db
 
-$con=new mysqli("http://mysql-43037-0.cloudclusters.net/","admin","6DCiZH15");
+$con=new mysqli("163.123.183.80","admin","6DCiZH15");
 if(!$con){
     // echo "db connection failed";
 }
